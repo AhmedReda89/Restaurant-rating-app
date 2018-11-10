45c77bf53d74f77f633ac971f6365fe55cbae2c8
ahmedreda89 <e.ahmedreda@gmail.com>
2018-11-11 01:33:01 +0200

    feat: maps & responsiveness

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYXJlZGE4OSIsImEiOiJjam9hbXR5aHowdjNhM2tsZXB6Mzh1bWU2In0.2a5rZYJbqLMwBi0ht1Polw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
